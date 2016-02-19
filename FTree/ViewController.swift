5d42e23cdbe19c3229b7cded71c1f48c84f2a649
David Boesen <davidboesen@davids-mac.local>
2016-02-18 20:09:00 -0800

    after alert box just need the other side

--- FTree/ViewController.swift
+++ FTree/ViewController.swift
@@ -12,18 +12,41 @@ class ViewController: UIViewController {
 
     @IBAction func NextView(sender: UIButton) {
         
-        navigationController?.title = ""
-        navigationItem.title = sender.currentTitle
-        
-        
-        
+   //     navigationController?.title = ""
+   //     navigationItem.title = sender.currentTitle
+        if sender.tag == 1 {
+            self.navigationController?.popViewControllerAnimated(true)
+        }
+        if sender.tag == 2 {
+            
+            // Create the alert controller
+            let alertController = UIAlertController(title: "End of Family Tree", message: "Would you like to start over?", preferredStyle: .Alert)
+            
+            // Create the actions
+            let okAction = UIAlertAction(title: "Restart", style: UIAlertActionStyle.Default) {
+                UIAlertAction in
+                self.navigationController?.popToRootViewControllerAnimated(true)
+            }
+            let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) {
+                UIAlertAction in
+               // do nothing for now
+            }
+            
+            // Add the actions
+            alertController.addAction(okAction)
+            alertController.addAction(cancelAction)
+            
+            // Present the controller
+            self.presentViewController(alertController, animated: true, completion: nil)
+
+            
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-    
+
     
     }
 
