8428e90778a2e531259785b03be6119f5a922702
David Boesen <davidboesen@davids-mac.local>
2016-02-18 21:22:31 -0800

    constraints round 1

--- FTree/ViewController.swift
+++ FTree/ViewController.swift
@@ -11,14 +11,23 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func NextView(sender: UIButton) {
-        
+    
+        // not used .... yet probably should have added a title per view conbtroller
    //     navigationController?.title = ""
    //     navigationItem.title = sender.currentTitle
+        
+        
+        
+        // I left the 2 dedunant view controllers in there just for the HW - else they are duplicated
+        
+        
+        
         if sender.tag == 1 {
+            //back button if parent button pressed
             self.navigationController?.popViewControllerAnimated(true)
         }
         if sender.tag == 2 {
-            
+            // Ask if you want to restart
             // Create the alert controller
             let alertController = UIAlertController(title: "End of Family Tree", message: "Would you like to start over?", preferredStyle: .Alert)
             
